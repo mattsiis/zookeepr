cfde9de5bb7b5d02c862642f0e893fcf3fd83ffc
mattsiis <35969076+mattsiis@users.noreply.github.com>
2021-09-01 23:42:10 -0400

    complete query section

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const { animals } = require('./data/animals');
-const PORT = process.env.PORT || 80;
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
@@ -38,6 +38,11 @@ function filterByQuery(query, animalsArray) {
     return filteredResults;
 }
 
+function findById(id, animalsArray) {
+    const result = animalsArray.filter(animal => animal.id === id)[0];
+    return result;
+}
+
 app.get('/api/animals', (req, res) => {
     let results = animals;
     if (req.query) {
@@ -45,6 +50,16 @@ app.get('/api/animals', (req, res) => {
     }
     res.json(results);
 })
+
+app.get('/api/animals/:id', (req, res) => {
+    const result = findById(req.params.id, animals);
+    if (result) {
+        res.json(result);
+    } else {
+        res.sendStatus(404);
+    }
+})
+
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
