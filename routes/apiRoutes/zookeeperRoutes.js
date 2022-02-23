ae1f8830014c40df08812abd2f2d80275897036e
mattsiis <35969076+mattsiis@users.noreply.github.com>
2022-02-22 23:40:19 -0500

    final commit

--- routes/apiRoutes/zookeeperRoutes.js
+++ routes/apiRoutes/zookeeperRoutes.js
@@ -26,9 +26,7 @@ router.get("/zookeepers/:id", (req, res) => {
 });
 
 router.post("/zookeepers", (req, res) => {
-    console.log(req.body.id);
     req.body.id = zookeepers.length.toString();
-    console.log(req.body.id);
 
     if(!validateZookeeper(req.body)) {
         res.status(400).send("The zookeeper is not properly formated.");
@@ -38,4 +36,14 @@ router.post("/zookeepers", (req, res) => {
     }
 });
 
+router.post('/animals', (req, res) => {
+    // set id based on what the next index of the array will be
+    req.body.id = animals.length.toString();
+    if(!validateAnimal(req.body)) {
+        res.status(400).send('The animal is not properly formatted.');
+    } else {
+        const animal = createNewAnimal(req.body, animals);
+        res.json(animal);
+    }
+})
 module.exports = router;
